8d72444b34e03b1cdb3b37f2dcd44296d96f440e
Sayan Samanta <samantasayan23@gmail.com>
2022-11-14 11:43:15 +0530

    chakra ui and router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from './components/Navbar';
+import Home from './views/Home';
+import Team from './views/Team';
+import Footer from './components/Footer';
 
 function App() {
     return (
-        <>
-        
-        </>
+        <ChakraProvider>
+            <Navbar />
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/organisers" element={<Team />} />
+                </Routes>
+            </Router>
+            <Footer />
+        </ChakraProvider>
     );
 }
 
