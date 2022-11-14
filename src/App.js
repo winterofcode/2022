56c0af3738d58aec6f894b819b12242ab51a3f01
Sayan Samanta <samantasayan23@gmail.com>
2022-11-14 12:02:44 +0530

    added suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import React, { Suspense } from 'react';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Navbar from './components/Navbar';
-import Home from './views/Home';
-import Team from './views/Team';
 import Footer from './components/Footer';
+
+const Home = React.lazy(() => import('./views/Home'));
+const Team = React.lazy(() => import('./views/Team'));
 
 function App() {
     return (
         <ChakraProvider>
             <Navbar />
             <Router>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/organisers" element={<Team />} />
-                </Routes>
+                <Suspense>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/organisers" element={<Team />} />
+                        <Route
+                            path="*"
+                            element={<Navigate to="/" replace />}
+                        />
+                    </Routes>
+                </Suspense>
             </Router>
             <Footer />
         </ChakraProvider>
