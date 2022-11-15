7135957dc50f373cc756210fec4d27c701b0530a
debojit <saha05debojit@gmail.com>
2022-11-15 20:52:05 +0530

    Card Error

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Program from "./About Data/Program";
+import Statistics from "./About Data/Statistics";
+
+const About = () => {
+  return (
+    <>
+      <Program />
+      <Statistics />
+    </>
+  );
+};
+
+export default About;
