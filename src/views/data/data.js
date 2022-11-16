c6e427887d0b82d3a07c43b73b19398f0f5150fd
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2022-11-17 00:38:49 +0530

    Update data.js

--- src/views/data/data.js
+++ src/views/data/data.js
@@ -1,5 +1,5 @@
 export const data = [
-    {name: "tester alpha", desg: "web dev mem", img: "https://git.dscnsec.com/people/2024/biswarghya.jpg", socialL: [{ isPres: "GIT", Slink: "https://www.github.com"}, {isPres: "LINKEDIN", Slink: "https://www.linkedin.com"}]},
+    {name: "tester alpha", desg: "web dev mem", img: "https://i.ibb.co/F0d5DwT/960x0.webp", socialL: [{ isPres: "GIT", Slink: "https://www.github.com"}, {isPres: "LINKEDIN", Slink: "https://www.linkedin.com"}]},
     {name: "tester beta", desg: "web dev mem", img: "https://i.ibb.co/F0d5DwT/960x0.webp", socialL: [{ isPres: "GIT", Slink: "https://www.github.com"}, {isPres: "LINKEDIN", Slink: "https://www.linkedin.com"}, {isPres: "TWIT", Slink: "https://www.twitter.com"}]},
     {name: "tester alpha", desg: "web dev mem", img: "https://i.ibb.co/F0d5DwT/960x0.webp", socialL: [{ isPres: "GIT", Slink: "https://www.github.com"}, {isPres: "LINKEDIN", Slink: "https://www.linkedin.com"}]},
     {name: "tester beta", desg: "web dev mem", img: "https://i.ibb.co/F0d5DwT/960x0.webp", socialL: [{ isPres: "GIT", Slink: "https://www.github.com"}, {isPres: "LINKEDIN", Slink: "https://www.linkedin.com"}, {isPres: "TWIT", Slink: "https://www.twitter.com"}]},
