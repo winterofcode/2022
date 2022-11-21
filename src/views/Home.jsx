656b80fc78ea8a59f890b25fc3e18c226126d847
Sayak Bhattacharjee <sayak718.s@gmail.com>
2022-11-22 05:11:59 +0530

    update

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,51 +10,74 @@ import layer_6 from "../assets/layer_6.webp"
 const Home = () => {
 
     const layer1 = useRef();
+    const layer2=useRef();
+    const layer3=useRef();
+    const layer4=useRef();
+    const layer5=useRef();
+    const layer6=useRef();
+    const title=useRef()
 
-    function sp() {
-        console.log("hit")
-        document.querySelectorAll("div.parallax__layer").forEach((shift, i) => {
-            console.log(i)
-            console.log(shift)
-            const x = (6 - i) / 2;
-            shift.style.transform = `translateZ(-100*${x}px) scale(${x}+1)`
-        })
-    }
+    // function sp() {
+    //     console.log("hit")
+    //     document.querySelectorAll("div.parallax__layer").forEach((shift, i) => {
+    //         console.log(i)
+    //         console.log(shift)
+    //         const x = (6 - i) / 2;
+    //         shift.style.transform = `translateZ(-100*${x}px) scale(${x}+1)`
+    //     })
+    // }
     useEffect(() => {
-        const obj = document.querySelector(".parallax")
-        console.log(obj)
         document.onmousemove = function (e) {
-            console.log(e.clientX, window.innerWidth);
+            // console.log(e.clientX, window.innerWidth);
+            const centre=window.innerWidth/2;
+            console.log((centre-e.clientX)/10)
+            const move1=(centre-e.clientX)/14
+            const move2=(centre-e.clientX)/12
+            const move3=(centre-e.clientX)/10
+            const move4=(centre-e.clientX)/7
+            const move5=(centre-e.clientX)/5
+            const move6=(centre-e.clientX)/4
+            const tmove=-1 * move5
+            // const title=(centre-e.clientX)/6
+            layer1.current.style.left=move1+"px"
+            layer2.current.style.left=move2+"px"
+            layer3.current.style.left=move3+"px"
+            layer4.current.style.left=move4+"px"
+            layer5.current.style.left=move5+"px"
+            layer6.current.style.left=move6+"px"
+            title.current.style.right=tmove+"px"
+    
+
         }
-        obj.addEventListener("scroll", sp)
+        // obj.addEventListener("scroll", sp)
     })
 
     return (
         <div className="home">
             <div className="parallax">
+            <h1 className="title" ref={title}>
+                        Winter of code</h1>
+                    <button className="registration-btn">Register Now</button>
                 <div ref={layer1} className="parallax__layer parallax__layer__0">
                     <img src={layer_0} />
                 </div>
-                <div className="parallax__layer parallax__layer__1">
+                <div className="parallax__layer parallax__layer__1" ref={layer1}>
                     <img src={layer_1}
                     />
                 </div>
-                <div className="parallax__layer parallax__layer__2">
+                <div className="parallax__layer parallax__layer__2" ref={layer2}>
                     <img src={layer_2} />
                 </div>
-                <div className="parallax__layer parallax__layer__3">
+                <div className="parallax__layer parallax__layer__3" ref={layer3}>
                     <img src={layer_3} />
                 </div>
-                <div className="parallax__layer parallax__layer__4">
+                <div className="parallax__layer parallax__layer__4" ref={layer4}>
                     <img src={layer_4} />
                 </div>
-                <div className="parallax__layer parallax__layer__5">
-                    <h1 className="title">
-                        Winter of code</h1>
-                    <button className="registration-btn">Register Now</button>
+                <div className="parallax__layer parallax__layer__5" ref={layer5}>
                     <img src={layer_5} />
                 </div>
-                <div className="parallax__layer parallax__layer__6">
+                <div className="parallax__layer parallax__layer__6" ref={layer6}>
                     <img src={layer_6} />
                 </div>
                 <div className="parallax__cover"></div>
