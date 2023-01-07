d1cbc9faee5b534199444b38ac55d5873dc54ed6
Sayan Samanta <samantasayan23@gmail.com>
2023-01-08 01:05:15 +0530

    merged home page banner temp

--- src/views/Newhome.jsx
+++ src/views/Newhome.jsx
@@ -4,31 +4,44 @@ import ltree from "../assets/ltree.webp"
 import rtree from "../assets/ritree.webp"
 import ltree_2 from "../assets/ltree-2.webp"
 import rtree_2 from "../assets/rtree-2.webp"
+import About from "../components/About";
+import Apply from "../components/Apply";
+import { Card } from "@chakra-ui/react";
+import Timeline from "../components/Timeline";
+import Footer from "../components/Footer";
 const NewHome=()=>{
     return(
-        <div className="nhome">
-            <div className="slide-down">Winter</div>
-            <div className="slide-lr">
-                <span className="slide-l">of </span>
-                <span className="slide-r">code</span>
+        <>
+            <div className="nhome">
+                <div className="slide-down">Winter</div>
+                <div className="slide-lr">
+                    <span className="slide-l">of </span>
+                    <span className="slide-r">code</span>
+                </div>
+                <div className="reg-btn">
+                <button className="wreg">Register Now</button>
+                </div>
+            
+                <div className="ltree-2">
+                    <img src={ltree_2} alt="" />
+                </div>
+                <div className="ltree">
+                    <img src={ltree} alt="" />
+                </div>
+                <div className="rtree-2">
+                    <img src={rtree_2} alt="" />
+                </div>
+                <div className="rtree">
+                    <img src={rtree} alt="" />
+                </div>
             </div>
-            <div className="reg-btn">
-            <button className="wreg">Register Now</button>
-            </div>
-           
-            <div className="ltree-2">
-                <img src={ltree_2} alt="" />
-            </div>
-            <div className="ltree">
-                <img src={ltree} alt="" />
-            </div>
-            <div className="rtree-2">
-                <img src={rtree_2} alt="" />
-            </div>
-            <div className="rtree">
-                <img src={rtree} alt="" />
-            </div>
-        </div>
+            <About />
+            <Apply />
+            <Card />
+            <Timeline />
+            {/* <Faq /> */}
+            <Footer />
+        </>
     )
 }
 export default NewHome;
